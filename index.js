568487a93c179167441b1d87eff28f4716505d7b
AlexHe <alexho@tapgo.cc>
2019-01-21 17:13:32 +0800

    LineBot

--- index.js
+++ index.js
@@ -1,47 +1,60 @@
-const express = require('express');
-const { ConsoleBot } = require('bottender');
-const app = express();
-//const { cwbcrawler } = require('./cwbcrawler');
+const { LineBot, LineHandler } = require('bottender');
+const { createServer } = require('bottender/express');
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .send('Hello, world!')
-    .end();
+const bot = new LineBot({
+  channelSecret: '03b7de370e0d852fe25b7b0e3b8f16f7',
+  accessToken: 'yIVA22uhyV5bjZeuM1VdeTCxj3idljOSBPdUcGMpDrbVzYAMkbqwh1y1EzLlLFUpIjnG9J+tsvvgkyFUP6dxshykZw60hu9QNnn8On+bBX7uSzKmzfJhVg4WP4FVhy5N9uKGjnkxSFMuCKGLHQC98QdB04t89/1O/w1cDnyilFU=',
 });
 
-const server = app.listen(process.env.PORT || 8080, () => {
-  const host = server.address().address;
-  const port = server.address().port;
+const handler = new LineHandler()
+  .onText(/yo/i, async context => {
+    await context.sendText('Hi there!');
+  })
+  .onEvent(async context => {
+    await context.sendText("I don't know what you say.");
+  })
+  .onError(async context => {
+    await context.sendText('Something wrong happened.');
+  });
 
-  console.log(`Example app listening at http://${host}:${port}`);
+bot.onEvent(handler);
+
+const server = createServer(bot);
+server.listen(8080, () => {
+  console.log('server is running on 8080 port...');
 });
+// const server = app.listen(process.env.PORT || 8080, () => {
+//   const host = server.address().address;
+//   const port = server.address().port;
 
-const bot = new ConsoleBot();
+//   console.log(`Example app listening at http://${host}:${port}`);
+// });
 
-function random(items) {
-  const randomIndex = Math.floor(
-    Math.random() * items.length
-  );
-  return items[randomIndex];
-}
+// const bot = new ConsoleBot();
 
-bot.onEvent(async context => {
-  //講笑話
-  if (context.event.text === '講笑話') {
-    await context.sendText(random([
-      '加油站最怕什麼樣的員工？油槍滑掉的員工',
-      '有一天，西瓜、榴蓮、奇異果一起出去玩，結果榴蓮不見了。因為榴蓮忘返',
-      '海記憶體知己，天涯若比鄰',
-    ]));
-  } else if (context.event.text === '天氣') {
-    var cwbcrawler = require("./cwbcrawler")
-    await context.sendText(
-      cwbcrawler,
+// function random(items) {
+//   const randomIndex = Math.floor(
+//     Math.random() * items.length
+//   );
+//   return items[randomIndex];
+// }
 
-      JSON.stringify(cwbcrawler)
-    );
-  }
-});
+// bot.onEvent(async context => {
+//   //講笑話
+//   if (context.event.text === '講笑話') {
+//     await context.sendText(random([
+//       '加油站最怕什麼樣的員工？油槍滑掉的員工',
+//       '有一天，西瓜、榴蓮、奇異果一起出去玩，結果榴蓮不見了。因為榴蓮忘返',
+//       '海記憶體知己，天涯若比鄰',
+//     ]));
+//   } else if (context.event.text === '天氣') {
+//     var cwbcrawler = require("./cwbcrawler")
+//     await context.sendText(
+//       cwbcrawler,
 
-bot.createRuntime();
+//       JSON.stringify(cwbcrawler)
+//     );
+//   }
+// });
+
+// bot.createRuntime();
