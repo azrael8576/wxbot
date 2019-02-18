5a97a32c48ba6dc5cd8b051bc6e2d63d689b18b2
azrael8576 <azrael8576love2008@gmail.com>
2019-02-18 15:50:10 +0800

    修改line bot位置

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ module.exports = {
     chatroomPlatform: process.env.chatroomPlatform,
     // line
     channelId: process.env.channelId,
-    channelSecret: '03b7de370e0d852fe25b7b0e3b8f16f7',
-    channelAccessToken: 'yIVA22uhyV5bjZeuM1VdeTCxj3idljOSBPdUcGMpDrbVzYAMkbqwh1y1EzLlLFUpIjnG9J+tsvvgkyFUP6dxshykZw60hu9QNnn8On+bBX7uSzKmzfJhVg4WP4FVhy5N9uKGjnkxSFMuCKGLHQC98QdB04t89/1O/w1cDnyilFU=',
+    channelSecret: 'a60164062edb9a0f03ac4126287c8567',
+    channelAccessToken: 'k++MdI5vyxY7QXpeYHnMPUGx1gmTtllw4rvbU+YDsB5J+/Zl4ecCCPtDG2kS+5x23+0NT4AX4hhwfcshZGOtMn4DliWS2/vr1Kb6CtiPY+M4vzJhb6WRHbd5eILINT/+DnXaQ+MMkluamggrgr/q1wdB04t89/1O/w1cDnyilFU=',
     // messenger
     messengerAccessToken: 'EAAER0eH61pgBAEb2yk7RnoOLwnplZA3D2Bik3NZBZAJorH3bHa4RWnbaMFxfrmjZCZCVQoqrlYbtQ0Fi33nkglmf4s0sqrr7IsTgNh2smDBCh09PJZCTf4bL8e1y1P6Me19jH8bz1oss3uQkZCZB54CS063iMrNZAQLZCka7zoJioUxgZDZD',
     messengerAppSecret: 'cb4f2d05f89008a573fef2c98bd1540d',
