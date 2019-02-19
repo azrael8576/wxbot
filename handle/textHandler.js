cca7e86e2bf005fe7316b39af7382d44c621e886
azrael8576 <azrael8576love2008@gmail.com>
2019-02-20 01:13:07 +0800

    FB & LINE API 完成,待整理

--- handle/textHandler.js
+++ handle/textHandler.js
@@ -7,6 +7,7 @@ const {
     platformReplyImage
 } = require("./crossPlatformHandle");
 const stations = require('../data/CWB_stations_location.json');
+const hourmap = require('../data/hourmap.json');
 var request = require('request-promise');
 
 
@@ -22,38 +23,41 @@ async function textHandle(context, text) {
     if (msg == 'hi' || msg == '嗨' || msg == '妳好' || msg == '你好') { replyMsg = 'Hello,我是你的個人助理. \n你可以輸入\'help\'查看我的指令'; }
     else if (msg == 'howareyou' || msg == 'howareyou?') { replyMsg = 'I am fine,You can enter \'help\' to view the command'; }
     else if (msg == '台鐵') { replyMsg = 'I am fine,You can enter \'help\' to view the command'; }
-    else if (msg == 'help') { replyMsg = '＊[City] （ex：台北市）\n目前暫時只支援台灣城市天氣\n請輸入完整「縣 or 市」\n＊[City]訂閱 （ex：台北市訂閱）\n訂閱明日天氣推播\n＊講笑話\n\n\n---------------------\n回饋 & Bug回報：https://goo.gl/forms/jy2EXg3G3bD9MGWl2 \n\n目前支援平台：LINE、Messenger'; }
-    else if (msg == '訂閱氣象') { replyMsg = "已為您訂閱明日\"台北\"氣象\n推播時間08:00\n" + context.platform + "\n" + context.session.user.id; }
+    else if (msg == 'help') { replyMsg = '＊[City] （ex：台北市）\n目前暫時只支援台灣城市天氣\n請輸入完整「縣 or 市」\n＊[City]訂閱[Hr]點 （ex：台北市訂閱15點）\n訂閱明日天氣推播\n請輸入完整含「點」\n＊講笑話\n\n\n---------------------\n回饋 & Bug回報：https://goo.gl/forms/jy2EXg3G3bD9MGWl2 \n\n目前支援平台：LINE、Messenger'; }
+    else if (msg == 'userId') { replyMsg = "已為您訂閱明日\"台北\"氣象\n推播時間08:00\n" + context.platform + "\n" + context.session.user.id; }
+    else if (msg == '續訂') { replyMsg = "已為您訂閱" }
     //爬蟲天氣
     for (const stid in stations) {
-        if (msg == stid) { replyMsg = await getAreaWeather(msg); }
-        else if (msg == stid + '訂閱') {
-            var platform = 0
-            if (context.platform == 'line') {
-                platform = 1;
-            }
-            else if (context.platform == 'messenger') {
-                platform = 2;
-            }
-            var myJSONObject = {
-                "userId": context.session.user.id,
-                "platform": platform,
-                "city": stid,
-                "time": "08:00"
-            };
-            await request({
-                url: "http://springboot-232107.appspot.com/subscriptions",
-                method: "POST",
-                json: true,   // <--Very important!!!
-                body: myJSONObject
-            }, function (error, response, body) {
-                if (!error && response.statusCode == 201) {
-                    replyMsg = '已為您訂閱明日"' + stid + "\"氣象\n推播時間08:00\n";
+        for (const timeS in hourmap) {
+            if (msg == stid) { replyMsg = await getAreaWeather(msg); }
+            else if (msg == stid + '訂閱' + timeS) {
+                var platform = 0
+                if (context.platform == 'line') {
+                    platform = 1;
                 }
-                else {
-                    replyMsg = '訂閱失敗';
+                else if (context.platform == 'messenger') {
+                    platform = 2;
                 }
-            });
+                var myJSONObject = {
+                    "userId": context.session.user.id,
+                    "platform": platform,
+                    "city": stid,
+                    "time": hourmap[timeS]
+                };
+                await request({
+                    url: "http://springboot-232107.appspot.com/subscriptions",
+                    method: "POST",
+                    json: true,   // <--Very important!!!
+                    body: myJSONObject
+                }, function (error, response, body) {
+                    if (!error && response.statusCode == 201) {
+                        replyMsg = '已為您訂閱\n明日"' + stid + "\"氣象\n推播時間" + hourmap[timeS];
+                    }
+                    else {
+                        replyMsg = '訂閱失敗';
+                    }
+                });
+            }
         }
     }
     //講笑話
--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const client = LineClient.connect({
   accessToken: config.channelAccessToken,
   channelSecret: config.channelSecret,
 });
+const facebook = require('fb-messenger-bot-api');
+const messageClient = new facebook.FacebookMessagingAPIClient(config.messengerAccessToken);
+
 
 const {
   getAreaWeather,
@@ -68,20 +71,43 @@ if (process.argv[2] == "console") {
   // 建立 Router 物件
   var router = express.Router();
   // 首頁路由 (http://localhost:8080)
-  router.get('/', async function () {
-    let userId = 'U0051e296713d9446d34a22013834ad81';
-    let msg = '臺北市';
+  router.post('/', async function (req, res) {
+    // var post = new Post()
+    var userId = req.body.userId
+    let msg = req.body.city;
+    let platform = req.body.platform;
+
     //爬蟲天氣
     for (const stid in stations) {
       if (msg == stid) { replyMsg = await getAreaWeather(msg); }
     }
+    if (platform == 1) {
+      client.push(userId, [
+        {
+          type: 'text',
+          text: "******訂閱推播******\n" + replyMsg,
+        },
+      ]);
+      res.send('success post to userId: ' + userId)
+    }
+    else if (platform == 2) {
+      messageClient.sendTextMessage(userId, "******訂閱推播******\n" + replyMsg + "\n\n＊FB因為政策關係請回覆我\"續訂\"")
+      res.send('success post to userId: ' + userId)
+    }
 
-    client.push(userId, [
-      {
-        type: 'text',
-        text: replyMsg,
-      },
-    ]);
+    // let userId = 'U0051e296713d9446d34a22013834ad81';
+    // let msg = '臺北市';
+    // //爬蟲天氣
+    // for (const stid in stations) {
+    //   if (msg == stid) { replyMsg = await getAreaWeather(msg); }
+    // }
+
+    // client.push(userId, [
+    //   {
+    //     type: 'text',
+    //     text: replyMsg,
+    //   },
+    // ]);
   });
   // 將路由套用至應用程式
   server.use('/post', router);
